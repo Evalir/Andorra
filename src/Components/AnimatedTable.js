b13ba9801d4fbd7c377a69d8798d3629bd1a72e8
Enrique Ortiz <hi@enriqueortiz.dev>
2019-11-04 01:05:59 -0400

    Add search functionality, styled components css prop support

--- src/Components/AnimatedTable.js
+++ src/Components/AnimatedTable.js
@@ -7,9 +7,10 @@ import {
   TableHeader,
   TableRow,
   TableCell,
-  Text,
   Badge,
+  IdentityBadge,
   theme,
+  useViewport,
 } from '@aragon/ui'
 
 const StyledLink = styled(Link)`
@@ -17,33 +18,40 @@ const StyledLink = styled(Link)`
   color: ${theme.accent};
 `
 
-const AnimatedTable = ({ items, title }) => (
-  <Table
-    header={
-      <TableRow>
-        <TableHeader title={title} />
-        <TableHeader title={'Miner'} />
-        <TableHeader title={'No. of transactions'} />
-      </TableRow>
-    }
-  >
-    {items.map((item, index) => (
-      <TableRow key={index}>
-        <TableCell>
-          <StyledLink to={`/blockinfo/${item.number}`}>
-            {item.number}
-          </StyledLink>
-        </TableCell>
-        <TableCell>
-          <Text>Miner {item.miner}</Text>
-        </TableCell>
-        <TableCell>
-          <Badge>{item.transactions.length} Transactions</Badge>
-        </TableCell>
-      </TableRow>
-    ))}
-  </Table>
-)
+const AnimatedTable = ({ items, title }) => {
+  const { above, breakpoints } = useViewport()
+  return (
+    <Table
+      header={
+        <TableRow>
+          <TableHeader title={title} />
+          <TableHeader title={'Miner'} />
+          {above(breakpoints.small) && (
+            <TableHeader title={'No. of transactions'} />
+          )}
+        </TableRow>
+      }
+    >
+      {items.map((item, index) => (
+        <TableRow key={index}>
+          <TableCell>
+            <StyledLink to={`/blockinfo/${item.number}`}>
+              {item.number}
+            </StyledLink>
+          </TableCell>
+          <TableCell>
+            <IdentityBadge entity={item.miner} />
+          </TableCell>
+          {above(breakpoints.small) && (
+            <TableCell>
+              <Badge>{item.transactions.length} Transactions</Badge>
+            </TableCell>
+          )}
+        </TableRow>
+      ))}
+    </Table>
+  )
+}
 
 AnimatedTable.propTypes = {
   title: PropTypes.string.isRequired,
