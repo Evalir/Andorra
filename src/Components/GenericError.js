46f758be7ba2d7b30c863ad3b5b7cd23154bf313
Enrique Ortiz <hi@enriqueortiz.dev>
2019-11-04 19:41:33 -0400

    Add error boundary

---
+++ src/Components/GenericError.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { EmptyStateCard, IconError } from '@aragon/ui'
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`
+
+const GenericError = ({ reportCallback }) => (
+  <Wrapper>
+    <EmptyStateCard
+      title="An unexpected error has ocurred"
+      text="We're sorry for the inconvenience."
+      icon={<IconError />}
+      actionText="Report Error"
+      onActivate={reportCallback}
+    />
+  </Wrapper>
+)
+
+GenericError.propTypes = {
+  reportCallback: PropTypes.func.isRequired,
+}
+
+export default GenericError
